dc6adb7ca68adff5b564ed98aed22054f1215f0b
jmayta73 <ingmaytae@hotmail.com>
2023-06-04 20:13:09 -0500

    funciones y app en react

---
+++ src/components/FuncComponent.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+function FuncComponent() {
+    const [value,setValue] = useState(0)
+    return (
+    <div>Componen function
+    <p><button onClick={()=>setValue(value-1)}>-</button> {value}<button onClick={()=>setValue(value+1)}>+</button> </p>
+    </div>
+    )
+}
+ 
+export default FuncComponent
